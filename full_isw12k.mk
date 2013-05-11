da0f1c028c13686a0b640830cb35f0d81901ad6d
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2013-05-11 13:34:17 +0900

    fujitsu -> kyocera

--- full_isw12k.mk
+++ full_isw12k.mk
@@ -9,6 +9,6 @@ $(call inherit-product, device/kyocera/isw12k/device.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := isw12k
 PRODUCT_NAME := full_isw12k
-PRODUCT_BRAND := fujitsu
+PRODUCT_BRAND := kyocera
 PRODUCT_MODEL := isw12k
-PRODUCT_MANUFACTURER := fujitsu
+PRODUCT_MANUFACTURER := kyocera
