02307e79220f342fc335b1b861a9955396436d14
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2013-05-12 08:54:14 +0900

    Mount userdata.img with overlayfs

--- root/init.mount_overlay.sh
+++ root/init.mount_overlay.sh
@@ -1,3 +1,5 @@
 #!/system/bin/sh
 
+#/system/xbin/losetup /dev/block/loop1 /data/b2g-userdata.img
 /system/bin/mount -t overlayfs -olowerdir=/system,upperdir=/b2g overlayfs /system
+/system/bin/mount -t overlayfs -olowerdir=/b2g-data,upperdir=/data overlayfs /data
